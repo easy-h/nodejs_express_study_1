d8e7d47fa0d873270cc7f9277bf56a3019a838cb
easy-h <jhlee7129@gmail.com>
2019-09-19 01:01:26 +0900

    static 파일 설정 및 url routing 설정, post, get 요청 처리 학습 (body-parser 설치)

--- app.js
+++ app.js
@@ -1,5 +1,36 @@
-var express = require('express');
+var express = require('express')
 var app = express()
+var bodyParser = require('body-parser')
 app.listen(3000, function() {
-    console.log("start node.js server on port 3000 !");
+    console.log("start node.js server on port 3000 !")
 });
+
+//static setting
+app.use(express.static('public'))
+
+//json 데이터를 받기 위한 post(클라이언트의 응답 json형태 및 Post 받기 설정)
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+//url routing
+app.get('/', function(req, res) {
+    console.log("/(root)")
+    //res.send("Hello world !")
+    //해당 파일을 클라이언트에게 전달
+    res.sendFile(__dirname + "/public/main.html")
+})
+
+app.get('/main', function(req, res) {
+    console.log("/main")
+    res.sendFile(__dirname + "/public/main.html")
+})
+
+app.post('/email_post', function(req, res) {
+    console.log(req.body.email)
+    //get -> req.param('email')
+    //post -> body.parser(npm install body-parser --save)
+    res.send('<h1>Welcome '+ req.body.email + '</h1>');
+})
+
+
+
